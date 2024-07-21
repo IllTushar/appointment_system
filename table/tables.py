018439d2301ec09d1348c0ab4570cec813e32cbc
illTushar <gtushar7668@gmail.com>
2024-07-21 22:19:30 +0530

    21.7.24x6

--- table/tables.py
+++ table/tables.py
@@ -1,5 +1,6 @@
 from engine.fastapi_engine import Base
-from sqlalchemy import Column, Integer, String, Date, CheckConstraint
+from sqlalchemy import Column, Integer, String, Date, CheckConstraint, Time, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Patient(Base):
@@ -8,6 +9,7 @@ class Patient(Base):
     first_name = Column(String(225))
     last_name = Column(String(225))
     DOB = Column(Date)
+    appointment = relationship('Appointment', back_populates='patient')
 
 
 '''
@@ -27,6 +29,7 @@ class Doctor(Base):
     __table_args__ = (
         CheckConstraint('rating > 0 AND rating <= 5', name='rating_check'),
     )
+    appointment = relationship('Appointment', back_populates='doctor')
 
 
 class Clinic(Base):
@@ -34,3 +37,20 @@ class Clinic(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(225), nullable=False)
     address = Column(String(225), nullable=False)
+    appointment = relationship('Appointment', back_populates='clinic')
+
+
+class Appointment(Base):
+    __tablename__ = 'Appointments'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    appointment_date = Column(Date, nullable=False)
+    appointment_time = Column(Time, nullable=False)
+    arrival_time = Column(Time, nullable=False)
+    complete_time = Column(Time)
+    active = Column(Boolean, default=True)
+    patient_id = Column(Integer, ForeignKey('Patients.id'), nullable=False)
+    doctor_id = Column(Integer, ForeignKey('Doctors.id'), nullable=False)
+    clinic_id = Column(Integer, ForeignKey('Clinic.id'), nullable=False)
+    patient = relationship('Patient', back_populates='appointments')
+    doctor = relationship('Doctor', back_populates='appointments')
+    clinic = relationship('Clinic', back_populates='appointments')
